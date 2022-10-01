fc4d34c833b795955aab5d595ceaa1deb43d63c9
Lidhya <lidhyajeejan@gmail.com>
2022-10-01 15:18:48 +0530

    cleaned

--- assets/js/main.js
+++ assets/js/main.js
@@ -244,11 +244,3 @@
   });
 
 })()
-
-/**
- * whatsapp control
- */
- const icon=document.querySelector('.eapp-whatsapp-chat-root-layout-component')
- window.addEventListener("resize", (e)=>{
-  icon.style.visibility="hidden"
- })
