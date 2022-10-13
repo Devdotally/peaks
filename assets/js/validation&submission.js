71392f51217848dc418e7b56769ef0f3041c712e
Lidhya <lidhyajeejan@gmail.com>
2022-10-13 11:54:04 +0530

    Testimonials and submission updated

---
+++ assets/js/validation&submission.js
@@ -0,0 +1,55 @@
+jQuery('#submit-form').validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 3,
+      normalizer: function (value) {
+        return $.trim(value);
+      }
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    subject: {
+      required: true
+    }
+  },
+  messages: {
+    name: {
+      required: "Name is required",
+      minlength: "Name should be atleast 3 characters"
+    },
+    email: {
+      required: "Email is required",
+      email: "Invalid email"
+    },
+    subject: "Subject is required",
+  },
+  submitHandler: function (form) {
+    $.ajax({
+      url: "https://script.google.com/macros/s/AKfycbwFz64M4lWGCQaS1PoTxuj3MPoxy5UX6KbvDDdnZTvs_1cQdRHxw-sfrRhxwC_KCarIkQ/exec",
+      method: "post",
+      data: $(form).serialize(),
+      success: (response) => {
+        Swal.fire({
+          title: 'Message Sent Successfully',
+          icon: 'success',
+          showCancelButton: false,
+          confirmButtonColor: '#3085d6',
+          confirmButtonText: 'OK'
+        })
+      },
+      error: (err) => {
+        Swal.fire({
+          title: 'Oops!',
+          text: 'Something went wrong, try again!',
+          icon: 'warning',
+          showCancelButton: false,
+          confirmButtonColor: '#e14343',
+          confirmButtonText: 'OK'
+        })
+      }
+    })
+  }
+})
